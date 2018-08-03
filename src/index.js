2a61d2cbaa51f917c968bda7579b5382cfa9c674
CodeDad <github email address>
2018-08-02 22:20:46 -0400

    created intial app.js file and index.html file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Home extends componenet {
+  render() {
+    return (
+      <div className="main">
+        <h1>Gallery</h1>
+      </div>
+    )
+  }
+}
+
+render(<Home/>, document.getElementById('container'));
