133fe6a0374e280ef7632cf58d510cc38d8d0673
CodeDad <github email address>
2018-08-02 23:25:13 -0400

    cleaned up extra brackets and fixed indents

--- src/index.js
+++ src/index.js
@@ -7,24 +7,20 @@ class Home extends Component {
   render() {
     return (
       <div className="main">
-          <h1>Galleria</h1>
+        <h1>Gallery</h1>
           <div className="gallery">
-              <CloudinaryContext cloudName="codedad">
-                  <div className="responsive">
-                      <div className="img">
-                          <a target="_blank" href={`https://res.cloudinary.com/codedad/image/upload/v1533259831/test-gallery/kitten-playing.gif`}>
-                              <Image publicId={'kittah.gif'}> </Image>
-                          </a>
-                      </div>
-                  </div>
-                          )
-                      })
-                  }
-              </CloudinaryContext>
-              <div className="clearfix"></div>
-          </div>
+            <CloudinaryContext cloudName="codedad">
+              <div className="responsive">
+                <div className="img">
+                  <a target="_blank" href={`https://res.cloudinary.com/codedad/image/upload/v1533259831/test-gallery/kitten-playing.gif`}>
+                    <Image publicId={'kitten-playing.gif'}> </Image>
+                  </a>
+                </div>
+              </div>
+            </CloudinaryContext>
+          <div className="clearfix"></div></div>
       </div>
-  );
+    );
   }
 }
 
